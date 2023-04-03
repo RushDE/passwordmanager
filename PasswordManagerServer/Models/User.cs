efcd5377fcf1546accb3741ac5ec53f48e9d9b0c
maximilian-forster <110381730+maximilian-forster@users.noreply.github.com>
2023-04-03 13:50:44 +0200

    Initial commit.

---
+++ PasswordManagerServer/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordManagerServer.Models
+{
+    public class User
+    {
+        public string Uuid { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
